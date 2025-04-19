5572235fc9f0cf7e2cf3ca688625ffbfe568fa84
Andrei D <codeandi@gmail.com>
2025-04-19 22:20:32 +0300

    added cors for local

--- invoices_nest/src/main.ts
+++ invoices_nest/src/main.ts
@@ -4,12 +4,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {abortOnError: false});
+  app.enableCors({
+    origin: 'http://localhost:5182',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  })
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3010);
 }
 bootstrap();
